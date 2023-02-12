d2f9885b54592af086d871ba977b586e865521f9
Kumar Shanu <kumar.shanu2020@vitbhopal.ac.in>
2023-02-12 20:42:31 +0530

    Made the patient schema and added the schema to the database using mongoose in the mongodb databasea and now we are ready to add our details while registration

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 // jshint esversion: 6
 const express = require("express");
-const Quote = require('inspirational-quotes');
+
+// require mongoose to setup database
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -10,18 +12,52 @@ app.use(function (req, res, next) {
   next();
 });
 
+
+// connection to mongoose local server at host 27017
+// and creating a database named Patient DataBase
+mongoose.connect('mongodb://localhost:27017/patientDB');
+
+// Making schema of the database patient
+const patSchema = {
+  name: String,
+  age: Number,
+  dob: Date,
+  address: String,
+  dailyCheckUp :[{
+    data: Date,
+    systolic: Number,
+    diastolic: Number,
+    sugar: Number,
+    height: Number,
+    weight: Number,
+    calCount: Number
+  }],
+  prescription:[{
+    doctorName: String,
+    date: Date,
+    medicines:[],
+    disease: String
+  }]
+};
+
+// Creating a collection model for the new schema created.
+// the collection name would ne pats
+const Pat = mongoose.model("Pat", patSchema);
+
 app.get("/", function (req, res) {
-  res.send(Quote.getQuote());
+  res.send("<h1>Hello World</h1>");
 });
 
 app.get("/tension", function (req, res) {
-  res.send("chal be tension mat de");
+  res.send("<h1>Chal be tension mat de</h1>");
 });
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8080;
 }
+
+
 app.listen(port, function () {
   console.log("Server started successfully");
 });
